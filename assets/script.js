2f53379a9b945eabc400d93eb1cc4ebf7e647c9d
Misachka <136197530+Misachka@users.noreply.github.com>
2023-08-19 18:45:42 -0400

    added render function

--- assets/script.js
+++ assets/script.js
@@ -84,4 +84,26 @@ function initPage() {
         searchHistory = [];
         renderSearchHistory();
     })
+    function renderSearchHistory() {
+        historyEl.innerHTML = "";
+        for (let i = 0; i < searchHistory.length; i++) {
+            const historyItem = document.createElement("input");
+            historyItem.setAttribute("type", "text");
+            historyItem.setAttribute("readonly", true);
+            historyItem.setAttribute("class", "form-control d-block bg-white");
+            historyItem.setAttribute("value", searchHistory[i]);
+            historyItem.addEventListener("click", function () {
+                getWeather(historyItem.value);
+            })
+            historyEl.append(historyItem);
+        }
+    }
+
+    renderSearchHistory();
+    if (searchHistory.length > 0) {
+        getWeather(searchHistory[searchHistory.length - 1]);
+    }
+    
 }
+
+initPage();
